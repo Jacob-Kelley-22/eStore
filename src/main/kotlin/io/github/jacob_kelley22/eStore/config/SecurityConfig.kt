9e20ba238cbd1998d6ca43b72f7aef781ff968c9
Jacob Kelley <jacobkelley922@gmail.com>
2026-04-21 23:41:22 -0400

    dockerized frontend & backend

--- src/main/kotlin/io/github/jacob_kelley22/eStore/config/SecurityConfig.kt
+++ src/main/kotlin/io/github/jacob_kelley22/eStore/config/SecurityConfig.kt
@@ -31,7 +31,10 @@ class SecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:5173")
+        configuration.allowedOrigins = listOf(
+            "http://localhost:5173",
+            "http://localhost:3000"
+            )
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
